116d02fd7a1d8222e262c0c6481d4700adea9ab8
ybdesire <ybdesire@gmail.com>
2019-12-16 06:44:50 +0800

    get device info by uiautomator.

---
+++ uiautomator/get_device_info.py
@@ -0,0 +1,19 @@
+from uiautomator import device as d
+
+print(d.info)
+
+'''
+{'currentPackageName': 'com.mumu.launcher', 
+'displayHeight': 810, 
+'displayRotation': 0, 
+'displaySizeDpX': 960, 
+'displaySizeDpY': 540, 
+'displayWidth': 1440, 
+'productName': 'cancro', 
+'screenOn': True, 
+'sdkInt': 23, 
+'naturalOrientation': True}
+'''
+
+
+
