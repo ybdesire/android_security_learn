4c3c0933988df5b6ba428688723bb01fdb4c0e87
ybdesire <ybdesire@gmail.com>
2019-12-19 06:53:24 +0800

    press home key

---
+++ uiautomator/press_home.py
@@ -0,0 +1,11 @@
+from uiautomator import device as d
+
+
+# press home key
+d.press.home()
+# press back key
+##d.press.back()
+# the normal way to press back key
+##d.press("back")
+# press keycode 0x07('0') with META ALT(0x02) on
+##d.press(0x07, 0x02)
